f45a3bc5b266b5e2c5447165d69199a76513a40e
Fiorella Román <125920877+Fiore0205@users.noreply.github.com>
2024-11-29 12:13:58 -0600

    modificar_torneo_y_liga

--- database/inserts.sql
+++ database/inserts.sql
@@ -2,15 +2,29 @@ use GestionVoley;
 
 INSERT INTO Usuario (id_Usuario, nombre_Usuario, contraseña_Usuario)
 VALUES 
-(2, 'MariaGomez', 'clave456'),
-(3, 'CarlosLopez', 'pass789'),
-(4, 'AnaMartinez', 'segura321');
+(5, 'MariaGomez', 'clave789');
 
-
-CALL sp_insertar_entrenador(2, 'María Gómez', 2);
+CALL sp_insertar_entrenador(5, 'Joaquin Morales', 5);
 CALL sp_insertar_entrenador(3, 'Carlos López', 3); 
 CALL sp_insertar_entrenador(4, 'Ana Martínez', 4);
 
 SELECT * FROM Entrenador;
 
-SELECT nombre_Torneo, posicion_Torneo FROM Torneo ORDER BY posicion_Torneo
+CALL sp_insertar_amistoso('Amistoso UCR-UNA', 3, 5);
+CALL sp_insertar_partido('Partido emocionante.','2-1','UCR Occidente','UCR Caribe');
+CALL sp_insertar_partido('El equipo visitante dominó completamente.','0-3', 'UCR Caribe', 'UCR Siquirres');
+CALL sp_insertar_amistoso('Amistoso UCR', 3, 6);
+
+UPDATE Usuario
+SET nombre_Usuario = 'Andy Fuller'
+WHERE id_Usuario = 2;
+
+UPDATE Usuario
+SET nombre_Usuario = 'Felipe Jenkins'
+WHERE id_Usuario = 3;
+
+UPDATE Usuario
+SET nombre_Usuario = 'Patrick Lisby'
+WHERE id_Usuario = 4;
+
+
